c8e8cc077af0201e24315451d71698335c0723a2
sundar395 <manepallisai395@gmail.com>
2021-08-16 10:10:40 +0530

    hejhfdjs

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $cooperation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
